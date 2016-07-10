a31f1f5286cdba51940c969e2954a848ede9d8c0
ericksot <todd.erickson@wwt.com>
2016-07-09 20:08:43 -0700

    adding the latest endpoints for updating sensors

--- broadDeviceInfo.py
+++ broadDeviceInfo.py
@@ -6,8 +6,7 @@ import json
 baseUrl = 'http://ciscoHackathon2016.cloudapp.net:5000/event'
 #baseUrl = 'http://localhost:5000/event'
 token = 'pQHp-kTqHoT6jNGkrXJJspGRvF_ecBRs'
-deviceIds = ['280dc96f-1e6b-49ad-b2ff-59897775f4cb']
-#, 'f46dd0b3-2dc7-402c-a5bc-d56ae8e39977', '46a4fa64-ac35-4142-9b0a-e62ef4ca2c8f', 'c7db7583-e019-4194-98c2-51a57e7fd728']
+deviceIds = ['280dc96f-1e6b-49ad-b2ff-59897775f4cb', 'f46dd0b3-2dc7-402c-a5bc-d56ae8e39977', '46a4fa64-ac35-4142-9b0a-e62ef4ca2c8f', 'c7db7583-e019-4194-98c2-51a57e7fd728']
 
 
 class socketSetup:
@@ -21,12 +20,12 @@ class socketSetup:
             data = {reading['meaning']: reading['value']}
             print(self.event)
             print(json.dumps(data))
-            requests.post(baseUrl, {'event': self.event, 'data': data}, headers={'contentType': 'application/json'})
+            requests.put('http://prod--hackathon2016--hackathon-api--bc5588.gce.shipped-cisco.com/sensors/' + self.event, {'name': self.event, 'value': data['count']}, headers={'Content-Type': 'application/json'})
 
 count = 1
 for deviceId in deviceIds:
     tempRelayr = RelayrService(token, deviceId)
-    socket = socketSetup('beeCountChange' + str(count))
+    socket = socketSetup('beeCount' + str(count))
     tempRelayr.startStream(socket.socketIO_callback)
     count += 1
 while True:
--- delegates/tempHackathonDelegate.py
+++ delegates/tempHackathonDelegate.py
@@ -2,7 +2,6 @@ from abstractRelayrDelegate import AbstractRelayrDelegate
 import json
 import time
 import requests
-from socketIO_client import SocketIO
 from pprint import pprint
 
 
@@ -11,7 +10,6 @@ class TempHackathonDelegate(AbstractRelayrDelegate):
 
     def __init__(self, credentials, publishing_period):
         super(TempHackathonDelegate, self).__init__(credentials, publishing_period)
-        self.socketIO = SocketIO('prod--hackathon2016--hackathon-api--bc5588.gce.shipped-cisco.com', 80)
         self.alertStatus = False
         self.alertThreshold = 60.0
         self.phoneNumbers = ['13145291038']
@@ -19,12 +17,21 @@ class TempHackathonDelegate(AbstractRelayrDelegate):
 
     def create_message(self):
         request_data = requests.get('http://guidoslabs.com/sensorFiles/temperature.txt')
+        #return self.base_message('temperature', float(99.9))
         return self.base_message('temperature', float(request_data.text))
 
     def socketIO_callback(self, data):
-        self.socketIO.emit('broadcast', {'event': 'hiveTempChange', 'data': data})
+        print 'calling1'
+        url = 'http://prod--hackathon2016--hackathon-api--bc5588.gce.shipped-cisco.com/sensors/hiveTemp'
+        #url = 'http://localhost:5000/sensors/hiveTemp'
+        print 'temperature: ' + str(data['temperature'])
+        mappedData = {'name': 'hiveTemp', 'value': data['temperature']}
+        requestResp = requests.put(url, mappedData, headers={'Content-Type': 'application/json'})
+        print 'requestResponse: ' + str(requestResp)
+        print 'calling2'
 
     def check_alert_status(self, data):
+        print 'temperature: ' + str(data['temperature'])
         if data['temperature'] <= self.alertThreshold and not self.alertStatus:
             alertAction = {'type': 'tropo', 'phoneNumbers': self.phoneNumbers, 'message': self.alertMessage}
             url = 'http://prod--hackathon2016--hackathon-api--bc5588.gce.shipped-cisco.com/actions'
